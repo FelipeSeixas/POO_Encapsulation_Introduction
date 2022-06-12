56aeea44109f18f0d7d73d8bb217777af6285ea3
Felipe Seixas <felipe.economista@hotmail.com>
2022-06-12 18:10:08 -0300

    Add files via upload

---
+++ EncapsulamentoAula6b.java
@@ -0,0 +1,27 @@
+package encapsulamentoaula6b;
+
+public class EncapsulamentoAula6b {
+
+    public static void main(String[] args) {
+
+      ControleRemoto c = new ControleRemoto();
+        
+//      c.ligar();
+
+      c.menosVolume();
+        
+      c.desligar();
+    
+      c.ligarMudo();
+      
+      c.desligarMudo();
+      
+      c.pause();
+      
+      c.play();
+        
+      c.abrirMenu();
+        
+    }
+    
+}
